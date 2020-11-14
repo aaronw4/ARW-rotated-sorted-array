67f251bb418058f3acd3b983d21449d197f8fe36
Aaron Williams <aaronrw4@yahoo.com>
2020-11-13 19:59:44 -0800

    Solution

---
+++ RSA.js
@@ -0,0 +1,15 @@
+var search = function(nums, target) {
+    const numsObj = {}
+
+    for (i = 0; i < nums.length; i++) {
+        let number = nums[i]
+        numsObj[number] = i
+    }
+
+    if (target in numsObj) {
+        return numsObj[target]
+    } else {
+        console.log(numsObj)
+        return -1
+    }
+};
